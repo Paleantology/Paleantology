972fb9e407288b9e1dbc07710f7611080b6c7c3d
April Wright <wright.aprilm@gmail.com>
2017-09-21 12:57:51 -0500

    my first python commit

---
+++ Teaching/my_first_script.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def extract_ants(ant_data, query_list):
+	'''Function to get ants out of dataframe'''
+	new_data = ant_data.loc[query_list]
+	return(new_data)
+	
+def get_names(namefile):
+	'''Read in a file of names'''
+	with open(namefile, 'r') as f:
+   		ant_names = f.read().splitlines() 
+	return(ant_names)
+
+def write_out():
+	''' '''
+
+if __name__ == "__main__":
+	ant_data = pd.read_csv("Data/Ants.csv", index_col=0)
+	namefile = "Data/AntNames.txt"
+	query_list = get_names(namefile)
+	extracted_ants = extract_ants(ant_data, query_list)
+	print(extracted_ants)
