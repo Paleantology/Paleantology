1e2b15a64209c1c0fc8a400630aefb1cd770dc8e
April Wright <wright.aprilm@gmail.com>
2018-12-16 23:08:28 -0600

    treecompare script

--- Scripts/TreeComparison.py
+++ Scripts/TreeComparison.py
@@ -1,61 +1,37 @@
-
-# coding: utf-8
-
-# In[31]:
+#!/anaconda3/bin/python3
 
 import dendropy
 from dendropy.calculate import treecompare
 import itertools
+import glob
+import sys
+import pandas as pd
+
+def read_trees(est_trees, comp_tree):
+	estimated_trees = []
+	list = glob.glob(est_trees)
+	for file in list:
+		estimated_trees.append(dendropy.Tree.get_from_path(file, schema="nexus", preserve_underscores=True, rooting='force-unrooted'))
+	print(estimated_trees[0])
+	comparison_tree = dendropy.Tree.get_from_path(comp_tree, schema="newick", preserve_underscores=True, rooting='force-unrooted', taxon_namespace= estimated_trees[0].taxon_namespace)
+	return(estimated_trees, comparison_tree)
 
 
-# In[163]:
+def perform_comparsions(treelist, comp_tree):
+	df = pd.DataFrame(columns=['RF'])
+	total_diffs = 2*len(comp_tree.nodes())
 
-estimated_trees = []
-mcc = dendropy.Tree.get_from_path("../Data/Morph/output/binary/combined/ants6.mcc.tre", schema="nexus", preserve_underscores=True, rooting='force-unrooted')
-map = dendropy.Tree.get_from_path("../Data/Morph/output/binary/combined/ants6.map.tre", schema="nexus", taxon_namespace= mcc.taxon_namespace, preserve_underscores=True, rooting='force-unrooted')
-con = dendropy.Tree.get_from_path("../Data/Morph/output/binary/combined/ants6.maj.tre", schema="nexus", taxon_namespace= mcc.taxon_namespace, preserve_underscores=True, rooting='force-unrooted')
-allc = dendropy.Tree.get_from_path("../Data/Morph/output/binary/combined/ants6.allcompat.tre", schema="nexus", taxon_namespace= mcc.taxon_namespace, preserve_underscores=True, rooting='force-unrooted')
-
-estimated_trees.append(mcc)
-estimated_trees.append(map)
-estimated_trees.append(con)
-estimated_trees.append(allc)
-
-
-
-# In[164]:
-
-barden_tree = dendropy.Tree.get_from_path("../Data/Morph/Data/Barden.nex", schema="nexus", preserve_underscores=True, rooting='force-unrooted')
-
-
-# In[165]:
-
-include_list = []
-blabels = barden_tree.taxon_namespace.labels()
-mcclabels = estimated_trees[1].taxon_namespace.labels()
-for label in mcclabels:
-    if label in blabels:
-        include_list.append(label)
-print(len(include_list))
-        
-for et in estimated_trees:
-    et.retain_taxa_with_labels(include_list)
-    print(len(et.leaf_edges()))
-
-
-# In[167]:
-
-total_diffs = 2*len(barden_tree.nodes())
-
-for et in estimated_trees:
-    for i in range(0,3):
-        et.migrate_taxon_namespace(barden_tree.taxon_namespace)
-        et.write_to_path("test{}.tre".format(i), schema="nexus")
-        print((treecompare.symmetric_difference(et,barden_tree))/total_diffs)
+	for et in treelist:
+        	et.migrate_taxon_namespace(comp_tree.taxon_namespace)
+        	df.loc[len(df)] = treecompare.symmetric_difference(et,comp_tree)/total_diffs
+        	print(treecompare.symmetric_difference(et,comp_tree)/total_diffs)
+	return(df)
     
-
-
-# In[ ]:
-
+if __name__ == "__main__":
+	est_trees = sys.argv[1]
+	comp_tree = sys.argv[3]
+	treelist, comparison_tree = read_trees(est_trees, comp_tree)
+	output = perform_comparsions(treelist, comparison_tree)
+	output.to_csv(sys.argv[2])
 
 
